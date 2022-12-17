e07331948defe7c0fa4aaddf85f857a8be277177
Sao I Kuan <saoikuan@gmail.com>
2022-12-17 13:50:00 -0500

    Initial Commit

---
+++ shLocation/shLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  shLocationApp.swift
+//  shLocation
+//
+//  Created by Cadence Kuan on 17/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct shLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
